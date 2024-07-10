d56f8b0dc0cda573d74c274a225aa6204ad1f2f1
gustavoJimenezz <60588577+gustavoJimenezz@users.noreply.github.com>
2024-07-10 16:39:30 -0300

    Update db.js

--- db/db.js
+++ db/db.js
@@ -3,7 +3,7 @@ const mysql = require("mysql2");
 const connection = mysql.createConnection({
     host:'localhost',
     user:'root',
-    password:'123',
+    password:'4994825',
     port: 3306,
 });
 
